c5a173df0dd1b1cf10df36d7fb3aea7d8edd6353
Navneet Kishan Srinivasan <115086283+NavneetKishanS@users.noreply.github.com>
2024-05-16 13:24:42 +0200

    PLSQL queries

---
+++ ARAMIS.sql
@@ -0,0 +1,9 @@
+CREATE TABLE emp2 AS SELECT * FROM VDANI.emp;
+CREATE TABLE dept2 AS SELECT * FROM VDANI.dept;
+
+SELECT * 
+FROM VDANI.E20;
+
+select * from kvflas.e10;
+
+grant select  on e10 to kvflas;
